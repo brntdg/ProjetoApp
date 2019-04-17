5104044e25b2163fcb74b976d54c62a01317fa8b
Bruno Santos <brntdg@live.com>
2019-04-17 14:15:59 -0300

    recebendo dados do usuario

--- App.js
+++ App.js
@@ -1,44 +1,57 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, TextInput, StyleSheet } from 'react-native';
 
+export default class ProjetoApp extends Component {
 
-export default class ProjetoApp extends Component {    
+  constructor(props) {
+    super(props);
+    this.state = {texto:''};
 
+    this.mudarTexto = this.mudarTexto.bind(this);
+  }
+
+  mudarTexto(t) {
+    let s = this.state;
+    if(t.length > 0) {      
+      s.texto = 'Olá, '+t;
+    } else {
+      s.texto = '';
+    }
+    
+
+    this.setState(s);
+  }
+ 
   render() {
 
-  
+    return(
 
-    return (
+      <View style={{paddingTop:30}}>
+        <TextInput style={styles.input} placeholder="Qual seu nome ?" onChangeText={this.mudarTexto}/>
 
-      <View style={styles.padrao}>
-        <Text style={styles.azulGrande}>Texto qualquer 1</Text>
-        <Text style={styles.vermelho}>Texto qualquer 2</Text>
-        <Text style={styles.verde}>Texto qualquer 3</Text>
-        <Text style={[styles.verde, styles.azulGrande]}>Texto qualquer 4</Text>
+        <Text style={styles.texto}>{this.state.texto}</Text>
+        
       </View>
 
-      );
+    );
+
 
   }
+
 
 }
 
 const styles = StyleSheet.create({
-  azulGrande: {
-    fontSize:30,
-    color:'#0000FF',
-    textAlign:'center'
+  input: {
+    height:40,
+    borderWidth:1,
+    borderColor:'#000000',
+    margin:10,
+    padding:10
   },
-  vermelho: {
+
+  texto: {
     fontSize:20,
-    color:'#FF0000',
-    textAlign:'right'
-  },
-  verde: {
-    fontSize:40,
-    color:'#00FF00'
-  },
-  padrao: {
-    paddingTop: 20
+    textAlign:'center'
   }
 });
