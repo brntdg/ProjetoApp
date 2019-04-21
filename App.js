4465dc3bf80fa4fadd8dfd05081823a4198ad934
Bruno Santos <brntdg@live.com>
2019-04-21 12:27:27 -0300

    criador de memes pt1

--- App.js
+++ App.js
@@ -1,61 +1,79 @@
 import React, { Component } from 'react';
-import { View, Text, Button, Alert, StyleSheet, TextInput } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
 
-export default class ProjetoApp extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {inputTexto:'' , texto:''}
-
-    this.apertouBotao = this.apertouBotao.bind(this);
-  }
-
-  apertouBotao() {
-    let s = this.state;
-    
-    if (s.inputTexto == 'Bruno') {
-      s.texto = 'Você Acertou!';
-    } else {
-      s.texto = 'Você Errou!';
-    }
-
-    this.setState(s);
-  }
+export default class ProjetoApp extends Component {  
 
   render() {
+    return ( 
+      <View style={styles.body}>
+        <View>
+          <Text style={styles.titulo}>Criador de Mimimi</Text>
+        </View>          
 
-    return(
-      <View style={{paddingTop:20}}>
-        <TextInput style={styles.input} placeholder="Adivinhe meu nome..." onChangeText={(inputTexto) => this.setState({inputTexto})}/>
+        <View style={styles.inputArea}>
+          <TextInput style={styles.input} placeholder="Digite seu mimimi"/>
+        </View>
 
-
-        <Button title="Aperte em mim." onPress={this.apertouBotao}/>
-
-        <Text style={styles.texto}>{this.state.texto}</Text>
-
+        <View style={styles.area}>
+          <Text style={[styles.texto, styles.texto1]}>Texto 1</Text>
+          <Image style={styles.guri} source={ require('./images/mimimi.jpg') } />
+          <Text style={[styles.texto, styles.texto2]}>Texto 2</Text>
+        </View>
 
       </View>
-
-      );
-
-
+    );
   }
-
-
-
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height:40,
-    borderWidth:1,
-    borderColor:'#000000',
-    padding:10,
-    margin:10
+  body:{
+    backgroundColor:'#999999',
+    paddingTop:30,
+    flex:1,
+    flexDirection:'column',
+    alignItems:'center'
+  }, 
+  titulo:{
+    fontSize:30,
+    color:'#FFFFFF'
   },
-
+  inputArea:{
+    alignSelf:'stretch'
+  },
+  input:{
+    borderWidth:1,
+    borderColor:'#999999',
+    backgroundColor:'#EEEEEE',
+    color:'#000000',
+    height:40,
+    padding:10,
+    margin:20
+  },
+  area:{
+    height:300,
+    width:300,
+    marginTop:10
+  },
+  guri:{
+    height:300,
+    width:300,
+    marginTop:-70,
+    zIndex:0
+  },
   texto:{
-    fontSize:20,
-    textAlign:'center'
+    fontSize:25,
+    color:'#FFFFFF',
+    padding:10,
+    backgroundColor:'transparent',
+    fontWeight:'bold',
+    textAlign:'center',
+    height:70
+  },
+  texto1:{
+    zIndex:1
+  },
+  texto2:{
+    zIndex:1,
+    marginTop:-70
   }
 });
