05f896be6fb6142a26e86bad3b663fe54373b9f4
Bruno Santos <brntdg@live.com>
2019-04-24 12:28:34 -0300

    Criador de memes pt2

--- App.js
+++ App.js
@@ -3,6 +3,36 @@ import { View, Text, StyleSheet, TextInput, Image } from 'react-native';
 
 export default class ProjetoApp extends Component {  
 
+  constructor(props) {
+    super(props);
+    this.state = {texto1:'', texto2:''}
+
+    this.escrever = this.escrever.bind(this);
+
+  }
+
+  mudarVogais(texto) {
+    let novoTexto = texto.toLowerCase();
+
+    novoTexto = novoTexto.replace(/(a|e|i|o|u)/g, 'i');
+    novoTexto = novoTexto.replace(/(à|á|â|ã)/g, 'i');
+    novoTexto = novoTexto.replace(/(è|é|ê)/g, 'i');
+    novoTexto = novoTexto.replace(/(ì|í|î)/g, 'i');
+    novoTexto = novoTexto.replace(/(ò|ó|ô|õ)/g, 'i');
+    novoTexto = novoTexto.replace(/(ù|ú|û|ü)/g, 'i');
+
+    return novoTexto;
+  }
+
+  escrever(t) {
+    let s = this.state;
+    s.texto1 = t;
+    s.texto2 = this.mudarVogais(t);
+    this.setState(s);
+  }
+
+
+
   render() {
     return ( 
       <View style={styles.body}>
@@ -11,13 +41,13 @@ export default class ProjetoApp extends Component {
         </View>          
 
         <View style={styles.inputArea}>
-          <TextInput style={styles.input} placeholder="Digite seu mimimi"/>
+          <TextInput style={styles.input} placeholder="Digite seu mimimi" onChangeText={this.escrever}/>
         </View>
 
         <View style={styles.area}>
-          <Text style={[styles.texto, styles.texto1]}>Texto 1</Text>
+          <Text style={[styles.texto, styles.texto1]}>{this.state.texto1.toUpperCase()}</Text>
           <Image style={styles.guri} source={ require('./images/mimimi.jpg') } />
-          <Text style={[styles.texto, styles.texto2]}>Texto 2</Text>
+          <Text style={[styles.texto, styles.texto2]}>{this.state.texto2.toUpperCase()}</Text>
         </View>
 
       </View>
@@ -57,7 +87,7 @@ const styles = StyleSheet.create({
   guri:{
     height:300,
     width:300,
-    marginTop:-70,
+    marginTop:-80,
     zIndex:0
   },
   texto:{
@@ -67,13 +97,13 @@ const styles = StyleSheet.create({
     backgroundColor:'transparent',
     fontWeight:'bold',
     textAlign:'center',
-    height:70
+    height:80
   },
   texto1:{
     zIndex:1
   },
   texto2:{
     zIndex:1,
-    marginTop:-70
+    marginTop:-80
   }
 });
