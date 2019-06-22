a6e459a1b00bbb054bc316611388d9d464c0cfd2
Bruno Santos <brntdg@live.com>
2019-06-22 00:35:43 -0300

    Tela de chat

--- App.js
+++ App.js
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Platform, FlatList } from 'react-native';
-import ListItem from './src/ListItem';
+import { View, StyleSheet, Platform, FlatList, ImageBackground } from 'react-native';
+import MsgItem from './src/MsgItem';
 
 export default class ProjetoApp extends Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props);
 		this.state = {
-			list:[
-				{key:'1', img:'https://www.b7web.com.br/avatar1.png', nome:'Jeferson Souza', msg:'Sai que horas ?'},
-				{key:'2', img:'https://www.b7web.com.br/avatar2.png', nome:'Bruno Santos', msg:'Não esquece a chave. Porque na semana passada você esqueceu do lado do sofá.'},
-				{key:'3', img:'https://www.b7web.com.br/avatar3.png', nome:'Mãe', msg:'Você não é todo mundo.'},
-				{key:'4', img:'https://www.b7web.com.br/avatar3.png', nome:'Andresa Silva', msg:'Estou com fome.'},
-				{key:'5', img:'https://www.b7web.com.br/avatar2.png', nome:'Leo Ciqueira', msg:'Vai jogar futebol hoje ?'},
-				{key:'6', img:'https://www.b7web.com.br/avatar1.png', nome:'Maedson Lopes', msg:'Ok! Boa Tarde.'},
+			chat:[
+				{key:'1', nome:'Bruno', msg:'Oi', m:true},
+				{key:'2', nome:'Jooh', msg:'Opa, tudo bem?', m:false},
+				{key:'3', nome:'Jooh', msg:'Como tá a vida?', m:false},
+				{key:'4', nome:'Bruno', msg:'Muito bem, graças a Deus. E você? eu soube que você tem um crush agora...', m:true},
+				{key:'5', nome:'Jooh', msg:'Pois é. Quase Namorado já. kkk', m:false},
+				{key:'6', nome:'Bruno', msg:'Maravilha! :)', m:true},
+				{key:'7', nome:'Bruno', msg:'Agora tenho que ir. Beijo!', m:true},
+				{key:'8', nome:'Jooh', msg:'Beijos!', m:false},
+				{key:'9', nome:'Jooh', msg:'Até mais!', m:false},
 			]
 		};
+
 	}
 
 	render() {
 
 		return (
 			<View style={styles.body}>
-				<FlatList 
-					data={this.state.list} 
-					renderItem={(item)=> <ListItem data={item} />} 
-				/>
+				<ImageBackground source={require('./assets/images/bg.jpg')} style={styles.chat}>
+					<FlatList 
+						data={this.state.chat}
+						renderItem={({item})=> <MsgItem data={item} />}
+					/>
+				</ImageBackground>
 			</View>
 		);
 	}
@@ -35,5 +41,8 @@ const styles = StyleSheet.create({
 	body:{
 		flex:1,
 		marginTop:(Platform.OS=='ios') ? 20 : 20
+	},
+	chat:{
+		flex:1
 	}
 });
